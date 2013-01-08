d85b5b3bfdcf22401dbbd256cb85c2fe7e6d4b95
sebcsaba <sebcsaba@gmail.com>
2013-01-08 23:44:36 +0100

    add getallheaders(), config_merge() and h() functions

--- functions.php
+++ functions.php
@@ -13,6 +13,20 @@ function I(&$x,$i,$default=null) {
 }
 
 /**
+ * Prints the given string encoded to html-safe.
+ * If other parameters are given, use sprintf() first to all parameters.
+ * 
+ * @param string $str
+ * @param mixed ...
+ */
+function h($str) {
+	if (func_num_args()>1) {
+		$str = call_user_func_array('sprintf', func_get_args());
+	}
+	print htmlspecialchars($str);
+}
+
+/**
  * Like func_get_args(), returns its invoker function call's parameters, but without the first item
  * 
  * @return array (mixed)
@@ -66,4 +80,44 @@ function implode_assoc(array $array, $betweenKeyAndValue, $beforeItems='', $afte
  */
 function is_empty($data) {
 	return empty($data);
+}
+
+/**
+ * Merges the given parameters to one associative array. When two
+ * arrays found at the same position, then they will be merged, too.
+ * 
+ * @param array $array1
+ * @param array ...
+ * @return array
+ */
+function config_merge(array $array1) {
+	$result = array();
+	foreach (func_get_args() as $param) {
+		foreach ($param as $key=>$value) {
+			if (is_array($value) && array_key_exists($key, $result) && is_array($result[$key])) {
+				$result[$key] = config_merge($result[$key], $value);
+			} else {
+				$result[$key] = $value;
+			}
+		}
+	}
+	return $result;
+}
+
+/**
+ * Defines getallheaders(), if needed.
+ * 
+ * @see http://php.net/manual/en/function.getallheaders.php
+ */
+if (!function_exists('getallheaders')) {
+	function getallheaders() {
+		$out = array();
+		foreach($_SERVER as $key=>$value) {
+			if ('HTTP_' == substr($key,0,5)) {
+				$key = str_replace(' ','-',ucwords(strtolower(str_replace('_',' ',substr($key,5)))));
+				$out[$key] = $value;
+			}
+		}
+		return $out;
+	}
 }
