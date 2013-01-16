e46915eb2632f0efa48b3459c5d4fc0b2d44e40d
sebcsaba <sebcsaba@gmail.com>
2013-01-16 22:57:14 +0100

    using transaction handling in RequestHandler

--- classes/core/RequestHandler.php
+++ classes/core/RequestHandler.php
@@ -29,6 +29,13 @@ class RequestHandler {
 	private $applicationHandler;
 	
 	/**
+	 * The database to run the transaction-handling on
+	 * 
+	 * @var Database
+	 */
+	private $database;
+	
+	/**
 	 * A Dependency Injector for instantiating new Action objects.
 	 * 
 	 * @var DI
@@ -41,32 +48,47 @@ class RequestHandler {
 	 * @param Config $config
 	 * @param AuthenticationService $authenticationService
 	 * @param ApplicationHandler $applicationHandler
+	 * @param Database $database
 	 * @param DI $di This will instantiate new Action objects.
 	 */
-	public function __construct(Config $config, AuthenticationService $authenticationService, ApplicationHandler $applicationHandler, DI $di) {
+	public function __construct(Config $config, AuthenticationService $authenticationService, ApplicationHandler $applicationHandler, Database $database, DI $di) {
 		$this->config = $config;
 		$this->authenticationService = $authenticationService;
 		$this->applicationHandler = $applicationHandler;
+		$this->database = $database;
 		$this->di = $di;
 	}
 	
 	/**
 	 * This does the work:
-	 * 1. parses the request
-	 * 2. creates forward
-	 * 3. process the given forward
-	 * 4. if new forward is given, go to step 2.
+	 * 1. opens a transaction
+	 * 2. authenticates the user
+	 * 3. parses the request
+	 * 4. creates forward
+	 * 5. process the given forward
+	 * 6. if new forward is given, go to step 4.
+	 * 7. commits the transaction
+	 * *. rollbacks the transaction when an exception occured
 	 */
 	public function run() {
-		// TODO start transaction
-		$user = $this->authenticationService->authenticate();
-		$request = $this->parseRequest($user);
-		$this->di->setInstance($request, 'Request');
-		$forward = $this->applicationHandler->determineInitialForward($request);
-		do {
-			$forward = $this->processForward($request, $forward);
-		} while (!is_null($forward));
-		// TODO commit/rollback transaction
+		$this->initializePhpEnvironment();
+		try {
+			$this->database->startTransaction();
+			$user = $this->authenticationService->authenticate();
+			$request = $this->parseRequest($user);
+			$this->di->setInstance($request, 'Request');
+			$forward = $this->applicationHandler->determineInitialForward($request);
+			do {
+				$forward = $this->processForward($request, $forward);
+			} while (!is_null($forward));
+			$this->database->commit();
+		} catch (Exception $ex) {
+			$this->database->rollback();
+			$forward = new ErrorForward($ex);
+			do {
+				$forward = $this->processForward($request, $forward);
+			} while (!is_null($forward));
+		}
 	}
 	
 	/**
@@ -89,6 +111,18 @@ class RequestHandler {
 	 */
 	private function processForward(Request $request, Forward $forward) {
 		return $forward->run($this->di, $this->config, $request);
+	}
+	
+	/**
+	 * Sets some PHP environment setting, depending on the current lifecycle.
+	 */
+	private function initializePhpEnvironment() {
+		if ('development'==$this->config->get('lifecycle')) {
+			ini_set('display_errors', 1);
+			error_reporting(E_ALL);
+		} else {
+			ini_set('display_errors', 0);
+		}
 	}
 	
 }
