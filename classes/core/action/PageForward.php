f4d575d72f62fb1373556d830390e160d46e521b
sebcsaba <sebcsaba@gmail.com>
2013-12-29 17:10:55 +0100

    Add ability to send X-Call-Javascript response header

--- classes/core/action/PageForward.php
+++ classes/core/action/PageForward.php
@@ -14,8 +14,17 @@ class PageForward implements Forward {
 	 */
 	private $page;
 	
-	public function __construct($page) {
+	/**
+	 * The name of the javascript function that should be called after the page is displayed.
+	 * (This should be handled by additional javascript)
+	 * 
+	 * @var string
+	 */
+	private $callJavascript;
+	
+	public function __construct($page, $callJavascript = null) {
 		$this->page = $page;
+		$this->callJavascript = $callJavascript;
 	}
 	
 	public function run(DI $di, Config $config, Request $request) {
@@ -23,6 +32,10 @@ class PageForward implements Forward {
 			$t = $di->get('TranslationHandler')->getTranslationFunction();
 		} else {
 			$t = function() { return 'No translations are supported. Set the "language/enabled" configuration value!'; };
+		}
+		if (!is_null($this->callJavascript)) {
+			$httpUtils = $di->get('HttpUtils');
+			header('X-Call-Javascript: '.$httpUtils->encodeHeader($this->callJavascript));
 		}
 		require_once('pages/'.$this->page.'.tpl.php');
 		return null;
