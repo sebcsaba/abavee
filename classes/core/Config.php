adde4e9a5fd5e16cd8cb7ce638603cdb9e6dcffb
sebcsaba <sebcsaba@gmail.com>
2013-12-29 10:18:27 +0100

    add defaultReturn optional parameter to Config.get

--- classes/core/Config.php
+++ classes/core/Config.php
@@ -28,14 +28,23 @@ class Config {
 	 * It will be splitted by the slashes, and for each part, the part will be
 	 * used as a key indexing the array found by the previous part.
 	 * 
+	 * If no value found at the given point, returns the default value if it's
+	 * not null, otherwise throws an exception.
+	 * 
 	 * @param string $path
+	 * @param mixed $returnDefault = null
 	 * @return mixed
+	 * @throws Exception
 	 */
-	public function get($path) {
+	public function get($path, $returnDefault = null) {
 		$result =& $this->data;
 		foreach (explode('/', $path) as $index) {
 			if (!array_key_exists($index, $result)) {
-				throw new Exception('no configuration value found: '.$path);
+				if (!is_null($returnDefault)) {
+					return $returnDefault;
+				} else {
+					throw new Exception('no configuration value found: '.$path);
+				}
 			}
 			$result =& $result[$index];
 		}
